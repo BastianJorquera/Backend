4f04697c27b061654126ddaf840c7ebdc04ebcee
BastianJorquera <bastian.jorquera.g@mail.pucv.cl>
2025-11-25 16:24:54 -0300

    corrección Frontend con Backend
    Se arregló el error de conexion entre backend y front. Se consolida conexion con base de datos de AWS

--- config/database.js
+++ config/database.js
@@ -7,8 +7,15 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         dialect: 'postgres',
         logging: false, // Puedes establecer a true para ver las consultas SQL en la consola
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // Perfecto para AWS RDS desde localhost
+            }
+        }
     }
 );
 
